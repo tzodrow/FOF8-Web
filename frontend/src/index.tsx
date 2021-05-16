c54b9e7702c2b7c1e6237a6dbd385ddd7d950bb2
Tim Zodrow <tszodr@gmail.com>
2021-05-15 20:28:36 -0500

    Migrate files over to tsx on frontend.

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router';
 import { store, history } from './reducers/store';
+import { App } from './App';
 
 ReactDOM.render(
     <React.StrictMode>
