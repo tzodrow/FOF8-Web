1e5ae1d77d1d8a523bc85394f9640ebd278d868f
Tim Zodrow <tszodr@gmail.com>
2021-05-16 13:21:24 -0500

    Update todo schema to allow for fields that are not defined.

--- backend/models/todos/todo.js
+++ backend/models/todos/todo.js
@@ -3,12 +3,14 @@
  */
 const mongoose = require('mongoose');
 
-const Todo = mongoose.model('Todo', {
-    text : {
+const todoSchema = new mongoose.Schema({
+    text: {
         type: String,
         trim: true,
         required: true
     }
-});
+}, { strict: false });
+
+const Todo = mongoose.model('Todo', todoSchema);
 
 module.exports = {Todo};
