898ddfdcb0b0b1ab2eb7a187ae354f240c20c4fa
Tim Zodrow <tszodr@gmail.com>
2021-05-15 20:47:31 -0500

    Update tsconfig to correct file.

--- frontend/src/components/AddTodo.tsx
+++ frontend/src/components/AddTodo.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 
 interface IAddTodoProps {
   handleAddTodo: (val: string) => void;
--- frontend/src/components/TodoList.tsx
+++ frontend/src/components/TodoList.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
 interface ITodoListProps {
   todos: Array<{text: string}>;
