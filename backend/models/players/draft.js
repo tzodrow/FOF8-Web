5dc8105bb6d96bba6098d35cb5e28deccf4837e4
Tim Zodrow <tszodr@gmail.com>
2021-05-18 08:37:43 -0500

    Remove todo logic.

--- backend/models/players/draft.js
+++ backend/models/players/draft.js
@@ -4,9 +4,22 @@ const draftSchema = new mongoose.Schema({
     Player_ID: {
         type: Number,
         trim: true,
-        required: true
+        required: true,
+    },
+    Interviewed: { 
+        type: Boolean
+    },
+    Low_Screen_Passes: {
+        type: Number
+    },
+    Low_Short_Passes: {
+        type: Number
     }
 }, { strict: false });
+
+// TODO: Figure out indexes
+// draftSchema.index({Low_Screen_Passes: 1, Low_Short_Passes: 1});
+// draftSchema.indexes();
 
 const Draft = mongoose.model('Draft', draftSchema);
 
