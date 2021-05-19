9becb05e570ae0c67dfee88504b263a87e22305d
Tim Zodrow <tszodr@gmail.com>
2021-05-18 23:53:05 -0500

    Clean up and remove FileDropzone.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,9 @@
-import { useState, useEffect } from 'react';
 import * as rax from 'retry-axios';
 import axios from "axios";
 import "./App.scss";
-import { FileDropzone } from './components/FileDropzone';
 import { IRecord } from './models/record';
+import { CSVReader } from 'react-papaparse';
+import { Parser, ParseResult } from 'papaparse';
 
 const myAxiosInstance = axios.create();
 myAxiosInstance.defaults.raxConfig = {
@@ -20,58 +20,7 @@ myAxiosInstance.defaults.raxConfig = {
 };
 const interceptorId = rax.attach(myAxiosInstance);
 
-const getRecords = (successCallback?: (data: any) => void, failureCallback?: () => void) => {
-  myAxiosInstance
-    .get("/api/player")
-    .then((response) => {
-      console.log(response.data);
-      if (successCallback) {
-        successCallback(response.data);
-      }
-    })
-    .catch((e) => {
-      console.log("Error : ", e);
-      if (failureCallback) {
-        failureCallback();
-      }
-    });
-}
-
-const saveRecord = (record: IRecord, successCallback?: () => void, failureCallback?: () => void) => {
-  myAxiosInstance
-    .post("/api/player/draft", record)
-    .then((res) => {
-      console.log(res);
-      if (successCallback) {
-        successCallback();
-      }
-    })
-    .catch((e) => {
-      console.log("Error : ", e);
-      if (failureCallback) {
-        failureCallback();
-      }
-    });
-}
-
-const saveRecords = (records: Array<IRecord>, successCallback?: () => void, failureCallback?: () => void) => {
-  myAxiosInstance
-    .post("/api/player/draftmany", records)
-    .then((res) => {
-      console.log(res);
-      if (successCallback) {
-        successCallback();
-      }
-    })
-    .catch((e) => {
-      console.log("Error : ", e);
-      if (failureCallback) {
-        failureCallback();
-      }
-    });
-}
-
-const upsertRecord = (record: IRecord, successCallback?: () => void, failureCallback?: () => void) => {
+const upsertRecord = (record: IRecord | Array<IRecord>, successCallback?: () => void, failureCallback?: () => void) => {
   myAxiosInstance
     .put("/api/player/upsert", record)
     .then((res) => {
@@ -87,64 +36,28 @@ const upsertRecord = (record: IRecord, successCallback?: () => void, failureCall
     });
 }
 
-const batchUpsert = (record: IRecord) => {
-  return myAxiosInstance.put("/api/player/upsert", record);
-}
-
 export function App() {
-  const [init, setInit] = useState(false);
-  const [records, setRecords] = useState<Array<IRecord>>([]);
-  const [savedRecords, setSavedRecords] = useState<Array<IRecord>>([]);
-  // const [insertManyCount, setInsertManyCount] = useState("10");
 
-  useEffect(() => {
-    if (!init) {
-      setInit(true);
+  const handleOnError = (err: any, file: any, inputElem: any, reason: any) => {
+    console.log(err)
+  }
 
-      // getRecords(
-      //   (data) => {
-      //     const sorted: Array<IRecord> = data.data.filter((_: IRecord, index: number) => index < 10).sort((a: IRecord, b: IRecord) => Number(a.Player_ID) - Number(b.Player_ID));
-      //     setSavedRecords(sorted);
-      //   },
-      //   () => setInit(false));
-    }
-  }, [init]);
+  const handleOnRemoveFile = (data: null) => {
+    console.log('---------------------------')
+    console.log(data)
+    console.log('---------------------------')
+  }
 
-  // const onChange = (e: React.FormEvent<HTMLInputElement>) => {
-  //   const newValue = e.currentTarget.value;
-  //   setInsertManyCount(newValue);
-  // }
-
-  // const onClick = () => {
-  //   const recordCount = Number(insertManyCount);
-  //   if (!isNaN(recordCount)) {
-  //     const insertManyRecords = records.splice(0, recordCount);
-  //     saveRecords(records, () => setSavedRecords([...savedRecords, ...insertManyRecords]));
-  //   }
-  // }
-
-  const upsertSavedRecord = (record: IRecord) => {
-    if (!savedRecords.some(sr => Number(sr.Player_ID) === Number(record.Player_ID))) {
-      setSavedRecords([...savedRecords, record]);
+  const onStep = (result: ParseResult<IRecord>, parser: Parser) => {
+    if (result?.data) {
+      parser.pause();
+      upsertRecord(result.data, () => parser.resume(), () => parser.abort());
     }
   }
 
-  const writeAllRecords = async (records: Array<IRecord>) => {
-    while(records.length > 0) {
-      const batch = records.splice(0, 200);
-      const upserts = batch.map(br => batchUpsert(br));
-      await Promise.all(upserts)
-        .then(res => {
-          console.log("Success Batch Insert.");
-        })
-        .catch(err => {
-          console.error(err);
-        })
-    }
-
-    // records.forEach(r => {
-    //   upsertRecord(r);
-    // });
+  const onComplete = (results: ParseResult<any>, file?: any) => {
+    console.log(results);
+    console.log(file);
   }
 
   return (
@@ -154,30 +67,21 @@ export function App() {
           <div className="col-xs-12 col-sm-8 col-md-8 offset-md-2">
             <h1>FOF8 Uploader</h1>
             <div className="fof8-app">
-              <FileDropzone loadRecords={writeAllRecords} />
-              {/* <div>
-                <input type={"text"} value={insertManyCount} onChange={onChange} />
-                <input type={"button"} value={"Insert Many"} onClick={onClick} />
-              </div> */}
-              <div className={"record-container"}>
-                <ul>
-                  {savedRecords.map((r, index) => {
-                    return (
-                      <li key={index}>{r.Player_ID} Saved</li>
-                    );
-                  })}
-                </ul>
-                <ul>
-                  {records.map((r, index) => {
-                    return (
-                      <li key={index}>
-                        <span>{r.Player_ID}</span>
-                        <input value={"Upsert"} type={"button"} onClick={() => upsertRecord(r, () => upsertSavedRecord(r))} />
-                      </li>
-                    );
-                  })}
-                </ul>
-              </div>
+              <CSVReader
+                onError={handleOnError}
+                style={{}}
+                config={{
+                  header: true,
+                  dynamicTyping: true,
+                  step: onStep,
+                  complete: onComplete,
+                  skipEmptyLines: true
+                }}
+                addRemoveButton
+                onRemoveFile={handleOnRemoveFile}
+              >
+                <span>Drop CSV file here or click to upload.</span>
+              </CSVReader>
             </div>
           </div>
         </div>
