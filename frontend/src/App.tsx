72a7024e7e1ae3a2558d515fdc41b7b501af8eaa
Tim Zodrow <tszodr@gmail.com>
2021-05-21 17:43:29 -0500

    Add example selector.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 
-import { useEffect, useState } from "react";
+import { MenuItem, Select } from "@material-ui/core";
+import React, { useEffect, useState } from "react";
 import { createLeague, getFileHistories, getLeagues, upsertFileHistory } from "./api/axiosApi";
 import "./App.scss";
 import { FileUpload } from "./components/FileUpload";
@@ -10,6 +11,11 @@ export function App() {
   const [leagues, setLeagues] = useState<Array<ILeague>>([]);
   const [fileHistories, setFileHistories] = useState<Array<IFileHistory>>([]);
   const [init, setInit] = useState(true);
+  const [age, setAge] = useState('');
+
+  const handleChange = (event: React.ChangeEvent<{ name?: string, value: unknown}>) => {
+    setAge(event.target.value as string);
+  };
 
   useEffect(() => {
     if (init) {
@@ -28,8 +34,20 @@ export function App() {
         <div className="row">
           <div className="col-xs-12 col-sm-8 col-md-8 offset-md-2">
             <h1>FOF8 Uploader</h1>
+            <div>
+            <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={age}
+                onChange={handleChange}
+              >
+                <MenuItem value={10}>Ten</MenuItem>
+                <MenuItem value={20}>Twenty</MenuItem>
+                <MenuItem value={30}>Thirty</MenuItem>
+              </Select>
+            </div>
             <div className="fof8-app">
-              <FileUpload title={"Player Record"}/>
+              <FileUpload title={"Player Record"} />
               <FileUpload title={"Player Ratings"} />
               <input type={"button"} value={"Create League"} onClick={() => createLeague({ CreateDate: new Date(), Name: "Test League", Active: true })} />
               <input type={"button"} value={"Create File History"} onClick={() => upsertFileHistory({ LeagueId: leagues.length > 0 && leagues[0]._id ? leagues[0]._id : "", Name: "Test History", UploadDate: new Date(), Completed: false })} />
