dd478c73799fce07239f86d83e8b4554b06c5eec
Tim Zodrow <tszodr@gmail.com>
2021-05-30 14:37:05 -0500

    Updates to left navigation.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,34 @@
 import { AppRouter } from "./AppRouter";
-import LeftNav from "./components/LeftNav";
 import { useAppSelector } from "./reducers/hooks";
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import { getLeagues } from "./api/axiosApi";
 import { useDispatch } from "react-redux";
 import { setLeague, setLeagues } from "./reducers/slices/leagueSlice";
 import { ILeague } from "./models/league";
 import { LeagueSelect } from "./components/LeagueSelect";
+import clsx from 'clsx';
+import { IconButton, makeStyles } from "@material-ui/core";
+import MenuIcon from '@material-ui/icons/Menu';
+import LeftNav from "./components/LeftNav";
 
 import "./App.scss";
 
+
+const useStyles = makeStyles((theme) => ({
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  hide: {
+    display: 'none',
+  }
+}));
+
 export function App() {
+  const classes = useStyles();
+
   const league = useAppSelector(state => state.league.league);
   const leagues = useAppSelector(state => state.league.leagues);
+  const [open, setOpen] = React.useState(false);
 
   const dispatch = useDispatch();
 
@@ -26,12 +42,31 @@ export function App() {
     }
   }
 
+  const handleDrawerOpen = () => {
+      setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+      setOpen(false);
+  };
+
   return (
     <div className={"fof8-app"}>
-      <LeftNav />
+      <LeftNav open={open} handleDrawerClose={handleDrawerClose} />
       <div className={"main-content"}>
         <div className={"main-header"}>
-          <h1>FOF8 Uploader</h1>
+          <div className={"icon-header"}>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              onClick={handleDrawerOpen}
+              edge="start"
+              className={clsx(classes.menuButton, open && classes.hide)}
+            >
+              <MenuIcon />
+            </IconButton>
+            <h1>FOF8 Uploader</h1>
+          </div>
           <LeagueSelect
             value={league}
             options={leagues}
