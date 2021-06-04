fbf9a817ea2359243c486b1929902461a12e1461
Tim Zodrow <tszodr@gmail.com>
2021-06-04 09:17:46 -0500

    Comment out empty resource.

--- main.tf
+++ main.tf
@@ -12,8 +12,8 @@ terraform {
 }
 
 # An example resource that does nothing.
-resource "null_resource" "example" {
-    triggers = {
-        value = "A example resource that does nothing!"
-    }
-}
+# resource "null_resource" "example" {
+#     triggers = {
+#         value = "A example resource that does nothing!"
+#     }
+# }
